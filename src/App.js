feb904e8d5e23974a3bde21108c4ec4b4c2ce34c
Rao Fahad <raofahad046@gmail.com>
2020-11-23 03:35:31 +0500

    played with react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Hello from './Hello';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App({name, age}) {
+  return <div>
+          <p>ello from App.js and my name is {name}
+          and my age is {age - 12}</p> <br />
+          <Hello fname={name}></Hello>
+         </div>
+         
 }
 
 export default App;
---
+++ src/Hello.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Hello.css'
+
+function Hello({fname}) {
+  return <div className="myname"> hello from hello.js {fname} </div>
+}
+
+export default Hello;
